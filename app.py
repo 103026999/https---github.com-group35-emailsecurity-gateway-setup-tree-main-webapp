ada7f4999e0aebd1032cb671bfb98c517509bdd1
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2023-08-02 20:06:19 +1000

    created read/write functions for emails/logs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 import csv
 import json
+import pickle
 from models.email import Email
 from models.log import Log
 
@@ -32,6 +33,64 @@ def csvToLogList(csvFilePath):
         #print(emailList)
         return logList
 
+def writeToBinaryFileFromEmailList(writeBinFilePath):
+    # Creat list and email objects
+    writeEmailList = []
+    email1 = Email(1,"asfasdf@gmail.com","user@group35.com","You have won the lottery"," 	Dear sir madam you have won the lottery please provide your bank login account to claim your winnings")
+    email2 = Email(2, "test@gmail.com","user@group35.com","This is a test"," 	Dear sir/madam this is a valid test email that is safe")
+    email3 = Email(3, "fasdfafdsf@gmail.com", "user@group35.com", "Please confirm PayPal deatils" ,"Dear sir please enter your paypal deatils")
+    email4 = Email(4, "group35@mail.com","bob@gmail.com","You have won the UK Lotto","Please provide your banking details for your lottery winnings")
+
+    # Append email objects to list
+    writeEmailList.append(email1)
+    writeEmailList.append(email2)
+    writeEmailList.append(email3)
+    writeEmailList.append(email4)
+
+    # Write list to bin file
+    with open(writeBinFilePath, "wb") as f:
+        pickle.dump(writeEmailList, f)
+
+def readFromBinaryFileToEmailList(readBinFilePath):
+    # Open binary file using pickle
+    with open(readBinFilePath, "rb") as f:
+        readEmailList = pickle.load(f)
+
+    for email in readEmailList:
+        emailOutput = str(email.id) +  " " + email.toAddress + " " + email.fromAddress + " " + email.subject + " " + email.body
+        #print(emailOutput)
+    
+    return readEmailList
+
+def writeToBinaryFileFromLogList(writeBinFilePath):
+    # Create list and log objects
+    writeLogList = []
+    log1 = Log(1,"2023-02-23", "11:12", "user@group35.com", "hacker@gmail.com", "Please provide paypal details", "Blocked")
+    log2 = Log(2,"2023-03-28", "14:32", "admin@group35.com", "user@group35.com", "This is a test email that is safe", "Allowed")
+    log3 = Log(3,"2023-04-18", "09:32", "user3@group35.com", "adfasdfads@gmail.com", "Please provide Click this link to get your winnings", "Blocked")
+    log4 = Log(4,"2023-05-17", "19:01", "user5@group35.com", "admin@group35.com", "Can you please meet me at lunchtime", "Allowed")
+
+    # Append log objects to list
+    writeLogList.append(log1)
+    writeLogList.append(log2)
+    writeLogList.append(log3)
+    writeLogList.append(log4)
+
+    # Write log list to file
+    with open(writeBinFilePath, "wb") as f:
+        pickle.dump(writeLogList, f)
+
+def readFromBinaryFileToLogList(readBinFilePath):
+    # Open binary file using pickle
+    with open(readBinFilePath, "rb") as f:
+        readLogList = pickle.load(f)
+
+    for log in readLogList:
+        logOutput = str(log.id) +  " " + log.date + " " + log.time + " " + log.toAddress + " " + log.fromAddress + " " + log.subject + " " + log.action
+        #print(logOutput)
+    
+    return readLogList
+        
 @app.route('/')
 def index():
     pieChartData = {'Task' : 'Total emails', 'Allowed' : 234, 'Blocked' : 153, 'Quarantine' : 86}
@@ -43,13 +102,15 @@ def index():
 @app.route('/emails')
 def emails():
     headings = ("ID","From","To","Subject","Body")
-    emailData = csvToEmailList('data/emails.csv')
+    writeToBinaryFileFromEmailList('data/emails.bin')
+    emailData = readFromBinaryFileToEmailList('data/emails.bin')
     return render_template('emails.html',emailData = emailData,headings = headings)
 
 @app.route('/logs')
 def logs():
     headings = ("ID","Date","Time","From","To","Subject","Action")
-    logData = csvToLogList('data/logs.csv')
+    writeToBinaryFileFromLogList('data/logs.bin')
+    logData = readFromBinaryFileToLogList('data/logs.bin')
     return render_template('logs.html', logData = logData, headings = headings)
 
 if __name__ == "__main__":
